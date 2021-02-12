6173364a42bf03fc354933a2f3cf9e4ede9ae045
lc <chenl15213gmail.com>
2021-02-13 01:14:43 +0800

    2020-0212 167 two ways done.

---
+++ src/L167.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class L167 {
+    public int[] twoSum1(int[] nums, int target) {
+        if(nums == null || nums.length < 2) return new int[2];
+
+        // Ascending Array! 2 pointers
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+            if(sum < target){
+                left++;
+            }
+            else if(sum > target) {
+                right--;
+            }
+            else {
+                // sum == target
+                // index of  result array start from 1
+                return new int[]{left+1, right+1};
+            }
+        }
+        return new int[2];
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        if(nums == null || nums.length < 2) return new int[2];
+
+        // Ascending Array! remember seen numbers using Map<value, index>.
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            // if target-nums[i] seen, get that seen number's index
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target-nums[i])+1, i+1};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+}
