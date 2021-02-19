7585dcc5bca1a5aaad859b8c631f166ffd45eb08
lc <chenl15213gmail.com>
2021-02-20 01:04:26 +0800

    20 done.

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -3,8 +3,6 @@ public class TreeNode {
      TreeNode left;
      TreeNode right;
 
-     private int i = 0;
-
      TreeNode() {}
      TreeNode(int val) { this.val = val; }
      TreeNode(int val, TreeNode left, TreeNode right) {
