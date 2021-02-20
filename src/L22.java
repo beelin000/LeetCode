5be9d25f8930f6d1226edcef37f964a0f4208002
LC <384412420@qq.com>
2021-02-21 01:31:00 +0800

    22,1438 to do.

---
+++ src/L22.java
@@ -0,0 +1,38 @@
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 1
+//输出：["()"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 8
+//
+// Related Topics 字符串 回溯算法
+// 👍 1565 👎 0
+
+
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class L22 {
+    public List<String> generateParenthesis(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
