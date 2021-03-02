7c7af9648f32b0153d9a2c0d6c22b11c9d71cfee
LC <384412420@qq.com>
2021-03-02 13:00:56 +0800

    58 done, 28 todo

--- src/L28.java
+++ src/L28.java
@@ -1,11 +1,19 @@
 class L28 {
     public int strStr(String h, String e) {
-        if(h == null || e == null) return -1;
-        if(e.length() == 0 ) return 0;
-        if(h.length() < e.length()) return -1;
+        if(h == null || e == null) {
+            return -1;
+        }
+        if(e.length() == 0 ) {
+            return 0;
+        }
+        if(h.length() < e.length()) {
+            return -1;
+        }
 
         a:for(int i=0; i<h.length(); i++){
-            if(i+e.length() > h.length()) return -1;
+            if(i+e.length() > h.length()) {
+                return -1;
+            }
             if(h.charAt(i) == e.charAt(0)) {
                 b:for(int j=0; i+j<h.length() && j<e.length(); j++) {
                     if(h.charAt(i+j) != e.charAt(j)) {
@@ -16,5 +24,19 @@ class L28 {
             }
         }
         return -1;
+    }
+
+    private int strStr2( String h, String e) {
+        if(h == null || e == null) {
+            return -1;
+        }
+        if(e.length() == 0 ) {
+            return 0;
+        }
+        if(h.length() < e.length()) {
+            return -1;
+        }
+
+
     }
 }
---
+++ src/L58.java
@@ -0,0 +1,43 @@
+//给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。
+//
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "Hello World"
+//输出：5
+//
+//
+// 示例 2：
+//
+//
+//输入：s = " "
+//输出：0
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 104
+// s 仅有英文字母和空格 ' ' 组成
+//
+// Related Topics 字符串
+// 👍 282 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class L58 {
+    public int lengthOfLastWord(String s) {
+        if(s==null || s.length() == 0) {
+            return 0;
+        }
+        String[] strings = s.split(String.valueOf(' '));
+        return strings.length > 0 ? strings[strings.length-1].length() : 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
