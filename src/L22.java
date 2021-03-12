3550a2f236f1807ef1e3d043fd8343a885f9df1e
LC <384412420@qq.com>
2021-03-13 01:33:51 +0800

    22 done with backtrack

--- src/L22.java
+++ src/L22.java
@@ -27,12 +27,44 @@
 // 👍 1565 👎 0
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class L22 {
     public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList();
+        dfs("", n ,n, res);
+        return res;
+    }
 
+    /**
+     * 回溯+剪枝
+     * @param cur 当前递归得到的结果
+     * @param left   左括号还有几个可以使用
+     * @param right  右括号还有几个可以使用
+     * @param res    结果集
+     */
+    private static void dfs(String cur, int left, int right, List<String> res) {
+        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+        if(left == 0 && right == 0) {
+            res.add(cur);
+            return;
+        }
+
+        // 剪枝（左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+        // 如： cur --> ")", "())"
+        if(left > right) {
+            return;
+        }
+
+        if(left > 0 ) {
+            dfs(cur + '(', left-1, right, res);
+        }
+
+        if(right > 0) {
+            dfs(cur + ')', left, right-1, res);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
