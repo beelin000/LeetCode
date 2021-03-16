a8ba5123a6999e0ebeea9040d9cf666504a4d6b9
LC <384412420@qq.com>
2021-03-16 23:32:05 +0800

    40 done

--- src/L40.java
+++ src/L40.java
@@ -34,6 +34,7 @@
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -43,10 +44,38 @@ class L40 {
         if(nums == null || nums.length == 0) {
             return res;
         }
+        Arrays.sort(nums);
 
+        bt(nums, target, 0, 0, new boolean[nums.length], new ArrayList<>(), res);
 
+        return res;
+    }
 
-        return ;
+    private static void bt(int[] nums, int target, int idx, int sum, boolean[] used,
+                           List<Integer> list, List<List<Integer>> res) {
+        if(sum == target) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        if(sum > target) {
+            return;
+        }
+
+        for(int i=idx; i< nums.length; i++) {
+            // used[i - 1] == true，说明同一树支candidates[i - 1]使用过
+            // used[i - 1] == false，说明同一树层candidates[i - 1]使用过
+            // 要对同一树层使用过的元素进行跳过
+            if(used[i] || i>0 && nums[i] == nums[i-1] && !used[i-1]) {
+                continue;
+            }
+            sum += nums[i];
+            used[i] = true;
+            list.add(nums[i]);
+            bt(nums, target, i+1, sum, used ,list, res);
+            list.remove(list.size()-1);
+            used[i] = false;
+            sum -= nums[i];
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
