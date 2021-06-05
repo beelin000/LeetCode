a4b1da578aafd8f686a5f94281cc98abae7146dd
LC <384412420@qq.com>
2021-06-06 00:44:19 +0800

    200 done

---
+++ src/L200.java
@@ -0,0 +1,79 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//
+// 此外，你可以假设该网格的四条边均被水包围。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 300
+// grid[i][j] 的值为 '0' 或 '1'
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+// 👍 1181 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class L200 {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    flip(grid, i , j);
+                }
+            }
+        }
+        return res;
+    }
+
+    // 向四周拓展
+    private void flip(char[][] g, int r ,int c) {
+        if ( r == g.length || c == g[0].length || r < 0 || c < 0) {
+            return;
+        }
+        if (g[r][c] == '1') {
+            g[r][c] = '0';
+            flip(g,r+1, c);
+            flip(g,r-1, c);
+            flip(g, r,c+1);
+            flip(g, r,c-1);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
